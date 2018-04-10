12af2816893a7e55f9edc9d84a52f333c88e489e
Sayed Khan <arsalon47@gmail.com>
2018-04-10 14:30:39 -0400

    working on genereate video item html

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,184 @@
 'use strict';
 const API_KEY = 'AIzaSyB67zJtiurAPJA3_l4k_dbafCQuTGautms';
 
+const placeHolder = {
+  'kind': 'youtube#searchListResponse',
+  'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/FIg-Ux-6mBojVoG6KEvNbLg53og"',
+  'nextPageToken': 'CAUQAA',
+  'regionCode': 'US',
+  'pageInfo': {
+    'totalResults': 1000000,
+    'resultsPerPage': 5
+  },
+  'items': [
+    {
+      'kind': 'youtube#searchResult',
+      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/D3mMdZJZYLaodYDNUJsnnzUF37E"',
+      'id': {
+        'kind': 'youtube#video',
+        'videoId': 'gjNukU-3jPk'
+      },
+      'snippet': {
+        'publishedAt': '2018-01-13T23:01:58.000Z',
+        'channelId': 'UCGBtk3J7PL40ZMhf3_1YBwg',
+        'title': 'Evolution of Batman in Cartoons in 45 Minutes (2018)',
+        'description': 'A look at how animated Batman has changed since 1968. Thanks for watching! Make sure to write your suggestions in the comments below. List of cartoons and voice actors: 1. Batman with Robin...',
+        'thumbnails': {
+          'default': {
+            'url': 'https://i.ytimg.com/vi/gjNukU-3jPk/default.jpg',
+            'width': 120,
+            'height': 90
+          },
+          'medium': {
+            'url': 'https://i.ytimg.com/vi/gjNukU-3jPk/mqdefault.jpg',
+            'width': 320,
+            'height': 180
+          },
+          'high': {
+            'url': 'https://i.ytimg.com/vi/gjNukU-3jPk/hqdefault.jpg',
+            'width': 480,
+            'height': 360
+          }
+        },
+        'channelTitle': 'Then & Now',
+        'liveBroadcastContent': 'none'
+      }
+    },
+    {
+      'kind': 'youtube#searchResult',
+      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/0-_uxwzuBIyue61TLvQNiDoixTA"',
+      'id': {
+        'kind': 'youtube#video',
+        'videoId': '4eQJpBwam1o'
+      },
+      'snippet': {
+        'publishedAt': '2018-03-27T21:23:08.000Z',
+        'channelId': 'UCl2Ae8IzmEusR43OL9HNcKQ',
+        'title': 'Batman: The Enemy Within - Episode 5 - Same Stitch (Villain Joker - Full Episode)',
+        'description': 'Batman: The Telltale Series Season 2 \\ Batman: The Enemy Within - Fifth and Final Episode - Full walkthrough with Joker as villain Episode 5 "Vigilante Joker" version: https://youtu.be/5TOPa8nYdNw...',
+        'thumbnails': {
+          'default': {
+            'url': 'https://i.ytimg.com/vi/4eQJpBwam1o/default.jpg',
+            'width': 120,
+            'height': 90
+          },
+          'medium': {
+            'url': 'https://i.ytimg.com/vi/4eQJpBwam1o/mqdefault.jpg',
+            'width': 320,
+            'height': 180
+          },
+          'high': {
+            'url': 'https://i.ytimg.com/vi/4eQJpBwam1o/hqdefault.jpg',
+            'width': 480,
+            'height': 360
+          }
+        },
+        'channelTitle': 'Batman Arkham Videos',
+        'liveBroadcastContent': 'none'
+      }
+    },
+    {
+      'kind': 'youtube#searchResult',
+      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/TsYf-CAQjEfbha38SwpzkM12gQ0"',
+      'id': {
+        'kind': 'youtube#video',
+        'videoId': 'q86Lx4uWI54'
+      },
+      'snippet': {
+        'publishedAt': '2018-03-07T16:00:04.000Z',
+        'channelId': 'UCyu8StPfZWapR6rfW_JgqcA',
+        'title': 'Justice League Action | Batman VS Batman! | DC Kids',
+        'description': 'Batman has been split into two: one passive and good, the other aggressive and mean! But the real question is, how will Green Arrow look in red? DCKids is home to all your favorite DC characters,...',
+        'thumbnails': {
+          'default': {
+            'url': 'https://i.ytimg.com/vi/q86Lx4uWI54/default.jpg',
+            'width': 120,
+            'height': 90
+          },
+          'medium': {
+            'url': 'https://i.ytimg.com/vi/q86Lx4uWI54/mqdefault.jpg',
+            'width': 320,
+            'height': 180
+          },
+          'high': {
+            'url': 'https://i.ytimg.com/vi/q86Lx4uWI54/hqdefault.jpg',
+            'width': 480,
+            'height': 360
+          }
+        },
+        'channelTitle': 'DC Kids',
+        'liveBroadcastContent': 'none'
+      }
+    },
+    {
+      'kind': 'youtube#searchResult',
+      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/zbgWjTr9rWilsjpSGCbioqY6iE4"',
+      'id': {
+        'kind': 'youtube#video',
+        'videoId': 'JUWgGzqxgQo'
+      },
+      'snippet': {
+        'publishedAt': '2017-09-23T15:00:01.000Z',
+        'channelId': 'UCyu8StPfZWapR6rfW_JgqcA',
+        'title': 'An Animated History of Batman | Batman Day | DC Kids',
+        'description': 'From Batman: The Animated Series to Justice League Action, check out the very best of Batman\'s animated history. DCKids is home to all your favorite DC characters, videos, comics, games...',
+        'thumbnails': {
+          'default': {
+            'url': 'https://i.ytimg.com/vi/JUWgGzqxgQo/default.jpg',
+            'width': 120,
+            'height': 90
+          },
+          'medium': {
+            'url': 'https://i.ytimg.com/vi/JUWgGzqxgQo/mqdefault.jpg',
+            'width': 320,
+            'height': 180
+          },
+          'high': {
+            'url': 'https://i.ytimg.com/vi/JUWgGzqxgQo/hqdefault.jpg',
+            'width': 480,
+            'height': 360
+          }
+        },
+        'channelTitle': 'DC Kids',
+        'liveBroadcastContent': 'none'
+      }
+    },
+    {
+      'kind': 'youtube#searchResult',
+      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/YpzG7OrUaR4ecAUfEduWHHC1EiA"',
+      'id': {
+        'kind': 'youtube#video',
+        'videoId': 'NwNdnAw61AA'
+      },
+      'snippet': {
+        'publishedAt': '2017-11-20T08:00:02.000Z',
+        'channelId': 'UCiZVMOinTQGb8HQu53VbV4Q',
+        'title': 'BATMAN: The Enemy Within Episode 3 All Cutscenes (Season 2) Fractured Mask | Game Movie 1080p 60FPS',
+        'description': 'FAR CRY 5 MOVIE (ALL CUTSCENES) - https://www.youtube.com/watch?v=-y4y8rHxjXE Everything Wrong With Episode 3: https://www.youtube.com/watch?v=0ztZqpj0z2U ▻ GLP Shirts and Merch: https://tinyur...',
+        'thumbnails': {
+          'default': {
+            'url': 'https://i.ytimg.com/vi/NwNdnAw61AA/default.jpg',
+            'width': 120,
+            'height': 90
+          },
+          'medium': {
+            'url': 'https://i.ytimg.com/vi/NwNdnAw61AA/mqdefault.jpg',
+            'width': 320,
+            'height': 180
+          },
+          'high': {
+            'url': 'https://i.ytimg.com/vi/NwNdnAw61AA/hqdefault.jpg',
+            'width': 480,
+            'height': 360
+          }
+        },
+        'channelTitle': 'Gamer\'s Little Playground',
+        'liveBroadcastContent': 'none'
+      }
+    }
+  ]
+};
+ 
 /*
   We want our store to hold a `videos` array of "decorated" objects - i.e. objects that
   have been transformed into just the necessary data to display on our page, compared to the large
@@ -11,7 +189,6 @@ const API_KEY = 'AIzaSyB67zJtiurAPJA3_l4k_dbafCQuTGautms';
     title: 'Cats dancing the Macarena',
     thumbnail: 'https://img.youtube.com/some/thumbnail.jpg'
   }
-
 */
 const store = {
   videos: []
@@ -32,10 +209,8 @@ const fetchVideos = function(searchTerm, callback) {
     key:'AIzaSyCdh_TJQJq0K9yKItkB9vVKNHULiO0zbg4',
     q: `${searchTerm}`,    
   };  
-  debugger;
-  $.getJSON(BASE_URL, query,callback);
+  $.getJSON(BASE_URL, query, callback);
 };
-
 // TASK:
 // 1. Create a `decorateResponse` function that receives the Youtube API response
 // 2. Map through the response object's `items` array
@@ -45,19 +220,27 @@ const fetchVideos = function(searchTerm, callback) {
 // TEST IT! Grab an example API response and send it into the function - make sure
 // you get back the object you want.
 const decorateResponse = function(response) {
+  return response.items.map(item => {
+    const id = item.id.videoId;
+    const title = item.snippet.title;
+    const thumbnail = item.snippet.thumbnails.default.url;
 
+    return {id, title, thumbnail};
+  });
 };
-
+const testVar = decorateResponse(placeHolder);
 // TASK:
 // 1. Create a `generateVideoItemHtml` function that receives the decorated object
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
 const generateVideoItemHtml = function(video) {
-  debugger;
-  console.log(video);
-
+  return `
+    <li value='${video.id}'>
+      <img src='${video.thumbnail}>
+      <h2>${video.title}</h2>
+    </li>`;
 };
-
+console.log(generateVideoItemHtml(testVar));
 // TASK:
 // 1. Create a `addVideosToStore` function that receives an array of decorated video 
 // objects and sets the array as the value held in store.items
@@ -90,6 +273,8 @@ const handleFormSubmit = function() {
   $('#YouTubeSearchForm').submit(function(event){
     console.log('got here - search form event ');
     event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('#search-term');
+    const query = queryTarget.val();
     fetchVideos($('#search-term').val(),generateVideoItemHtml);
     $('#search-term').val('');
   });  
